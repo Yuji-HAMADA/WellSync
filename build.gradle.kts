5745578f8969facd8d51e86bfb9beab37d9fdcc4
Yuji <aa366@hotmail.com>
2026-05-16 10:48:20 +0900

    build: update Gradle to 9.5.1 and KSP to 2.3.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("com.android.application") version "9.2.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
-    id("com.google.devtools.ksp") version "2.3.7" apply false
+    id("com.google.devtools.ksp") version "2.3.8" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
     id("androidx.room") version "2.8.4" apply false
     id("com.github.ben-manes.versions") version "0.54.0"
